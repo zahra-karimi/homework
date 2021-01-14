c74f81c7618124041f81710c43c1e289185fdd10
Zahra Karimi <thezahrakarimi1@gmail.com>
2021-01-14 18:22:58 +0430

    Initial commit

---
+++ lib/One.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class One extends StatefulWidget {
+  @override
+  _OneState createState() => _OneState();
+}
+
+class _OneState extends State<One> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text("Hello World! by Zahra Karimi"),);
+  }
+}
